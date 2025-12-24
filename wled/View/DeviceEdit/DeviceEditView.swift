d43e470dee53ca8d7e87da8f027bb2c34f736663
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-12-23 19:20:57 -0500

    Refactor: Add spacing and padding to DeviceEditView
    Wrapped the main content of `DeviceEditView` in a `VStack` with spacing to improve layout. Added bottom padding to the view to prevent content from being obscured by other UI elements.

--- wled/View/DeviceEdit/DeviceEditView.swift
+++ wled/View/DeviceEdit/DeviceEditView.swift
@@ -21,91 +21,94 @@ struct DeviceEditView: View {
 
     var body: some View {
         ScrollView {
-            GroupBox {
-                DeviceInfoTwoRows(device: device)
-            }
-            .groupBoxStyle(.device(color: device.currentColor))
-            .listRowInsets(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+            VStack(spacing: 10) {
+                GroupBox {
+                    DeviceInfoTwoRows(device: device)
+                }
+                .groupBoxStyle(.device(color: device.currentColor))
+                .listRowInsets(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
 
-            VStack(alignment: .leading) {
-                Text("Custom Name")
-                TextField("Custom Name", text: $viewModel.customName)
-                    .submitLabel(.done)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
+                VStack(alignment: .leading) {
+                    Text("Custom Name")
+                    TextField("Custom Name", text: $viewModel.customName)
+                        .submitLabel(.done)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
 
-            Toggle("Hide this Device", isOn: $viewModel.hideDevice)
-                .padding(.trailing, 2)
+                Toggle("Hide this Device", isOn: $viewModel.hideDevice)
+                    .padding(.trailing, 2)
+                    .padding(.bottom)
+
+                HStack {
+                    Text("Update Channel")
+                    Spacer()
+                    Picker("Update Channel", selection: $viewModel.branch) {
+                        ForEach(Branch.allCases.filter { $0 != .unknown }) { branch in
+                            Text(LocalizedStringKey(branch.nameKey))
+                                .tag(branch)
+                                .padding()
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    .fixedSize()
+                }
                 .padding(.bottom)
 
-            HStack {
-                Text("Update Channel")
-                Spacer()
-                Picker("Update Channel", selection: $viewModel.branch) {
-                    ForEach(Branch.allCases.filter { $0 != .unknown }) { branch in
-                        Text(LocalizedStringKey(branch.nameKey))
-                            .tag(branch)
-                            .padding()
-                    }
-                }
-                .pickerStyle(.segmented)
-                .fixedSize()
-            }
-            .padding(.bottom)
-
-            if (device.stateInfo != nil) {
-                // TODO: Update this to be its own "helicopter" view
-                VStack(alignment: .leading) {
-                    if ((device.availableUpdateVersion ?? "").isEmpty) {
-                        Text("Your device is up to date")
-                        Text(
-                            "Version \(device.stateInfo?.info.version ?? unknownVersion)"
-                        )
-                        HStack {
-                            Button(action: {
-                                Task {
-                                    await viewModel.checkForUpdate()
+                if (device.stateInfo != nil) {
+                    // TODO: Update this to be its own "helicopter" view
+                    VStack(alignment: .leading) {
+                        if ((device.availableUpdateVersion ?? "").isEmpty) {
+                            Text("Your device is up to date")
+                            Text(
+                                "Version \(device.stateInfo?.info.version ?? unknownVersion)"
+                            )
+                            HStack {
+                                Button(action: {
+                                    Task {
+                                        await viewModel.checkForUpdate()
+                                    }
+                                }) {
+                                    Text(viewModel.isCheckingForUpdates ? "Checking for Updates" : "Check for Update")
                                 }
-                            }) {
-                                Text(viewModel.isCheckingForUpdates ? "Checking for Updates" : "Check for Update")
-                            }
-                            .buttonStyle(.bordered)
-                            .padding(.trailing)
-                            .disabled(viewModel.isCheckingForUpdates)
-                            ProgressView()
-                                .opacity(viewModel.isCheckingForUpdates ? 1 : 0)
-                        }
-                    } else {
-                        HStack {
-                            Image(systemName: getUpdateIconName())
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 30.0, height: 30.0)
+                                .buttonStyle(.bordered)
                                 .padding(.trailing)
-                            VStack(alignment: .leading) {
-                                Text("Update Available")
-                                Text("From \(device.stateInfo?.info.version ?? unknownVersion) to \(device.availableUpdateVersion ?? unknownVersion)")
-                                NavigationLink {
-                                    DeviceUpdateDetails(device: device)
-                                } label: {
-                                    Text("See Update")
+                                .disabled(viewModel.isCheckingForUpdates)
+                                ProgressView()
+                                    .opacity(viewModel.isCheckingForUpdates ? 1 : 0)
+                            }
+                        } else {
+                            HStack {
+                                Image(systemName: getUpdateIconName())
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 30.0, height: 30.0)
+                                    .padding(.trailing)
+                                VStack(alignment: .leading) {
+                                    Text("Update Available")
+                                    Text("From \(device.stateInfo?.info.version ?? unknownVersion) to \(device.availableUpdateVersion ?? unknownVersion)")
+                                    NavigationLink {
+                                        DeviceUpdateDetails(device: device)
+                                    } label: {
+                                        Text("See Update")
+                                    }
                                 }
                             }
+                            .buttonStyle(.borderedProminent)
                         }
-                        .buttonStyle(.borderedProminent)
                     }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(8)
+                    .animation(.default, value: device.availableUpdateVersion)
+                    .animation(.default, value: viewModel.isCheckingForUpdates)
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-                .background(Color(UIColor.secondarySystemBackground))
-                .cornerRadius(8)
-                .animation(.default, value: device.availableUpdateVersion)
-                .animation(.default, value: viewModel.isCheckingForUpdates)
-            }
 
-            Spacer()
+                Spacer()
+            }
+            .padding()
+            .padding(.bottom, 100)
         }
-        .padding()
         .navigationTitle("Edit Device")
         .navigationBarTitleDisplayMode(.large)
     }
